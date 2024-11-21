126a1e98e05bcf9f35ebd110ccca28c0a4c6c277
yabuJunn <juancho04reyes@gmail.com>
2024-11-21 16:56:14 -0500

    Se buscan las cosas con el context

--- src/pages/dashboardPage/dashboardPage.tsx
+++ src/pages/dashboardPage/dashboardPage.tsx
@@ -1,41 +1,42 @@
-import './dashboardPage.css'
-
-import { useEffect, useState } from "react"
-import { fetchApi } from "../../services/fetch"
-import { CatApiResponseType } from "../../types/catApiTypes"
-import { NavigationBar } from '../../components/navigationBar/navigationBar'
-import { useDispatch, useSelector } from 'react-redux'
-import { updateCatsData } from '../../store/catsSlice/slice'
-import { RootState } from '../../store/store'
-import { CatCard } from '../../components/dashboardPage/catCard/catCard'
-
-import { SearchContext } from '../../context/searchContext'
+import './dashboardPage.css';
+import { useEffect, useState } from 'react';
+import { fetchApi } from '../../services/fetch';
+import { CatApiResponseType } from '../../types/catApiTypes';
+import { NavigationBar } from '../../components/navigationBar/navigationBar';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateCatsData } from '../../store/catsSlice/slice';
+import { RootState } from '../../store/store';
+import { CatCard } from '../../components/dashboardPage/catCard/catCard';
+import { SearchContext } from '../../context/searchContext';
 
 export const DashboardPage = () => {
-    const dispatch = useDispatch()
-    const [searchText, setSearchText] = useState('')
+    const dispatch = useDispatch();
+    const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
         const fetch = async () => {
-            const responseApi: CatApiResponseType = await fetchApi('https://api.thecatapi.com/v1/breeds')
-            dispatch(updateCatsData(responseApi))
-        }
+            const responseApi: CatApiResponseType = await fetchApi('https://api.thecatapi.com/v1/breeds');
+            dispatch(updateCatsData(responseApi));
+        };
 
-        fetch()
-    }, [dispatch])
+        fetch();
+    }, [dispatch]);
 
-    const catsSliceData = useSelector((state: RootState) => state.cats)
+    const catsSliceData = useSelector((state: RootState) => state.cats);
 
-    return <>
+    // Filtra los gatos según el texto de búsqueda
+    const filteredCats = catsSliceData.catsData.filter((cat) =>
+        cat.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    return (
         <SearchContext.Provider value={{ searchValue: searchText, setSearchValue: setSearchText }}>
-
-            <NavigationBar></NavigationBar>
-
-            <div id='catsCardsContainer'>
-                {catsSliceData.catsData.map((cat) => {
-                    return <CatCard catData={cat} key={cat.id}></CatCard>
-                })}
+            <NavigationBar />
+            <div id="catsCardsContainer">
+                {filteredCats.map((cat) => (
+                    <CatCard catData={cat} key={cat.id} />
+                ))}
             </div>
         </SearchContext.Provider>
-    </>
-}
+    );
+};
